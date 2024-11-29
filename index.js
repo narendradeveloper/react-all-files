0be7437256f18dede314d03dd1b2016051e02a4c
narendradeveloper <narendradeveloper>
2024-11-29 16:34:03 +0530

    app

---
+++ index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// import App from './App';
+import reportWebVitals from './reportWebVitals';
+// import Sample from './Sample';
+import Demo from './app1';      
+// import Home from './day2/home';
+// import Parent from './day3/parent';
+// import Card from './day4/card';
+// import Fun from './day5/draliing';
+
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <Demo/>
+    {/* <Fun/> */}
+    {/* <Card/> */}
+    {/* <Parent admin={true}/> */}
+    {/* <Home/> */}
+    {/* <Sample/> */}
+  </React.StrictMode>
+)
+
+reportWebVitals();
