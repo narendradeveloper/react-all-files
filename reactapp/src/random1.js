b04961aec0d1b7cbc75e882bcda2ed00fca1cae9
narendradeveloper <narendradeveloper>
2025-02-12 12:47:06 +0530

    jjhg

--- reactapp/src/random1.js
+++ reactapp/src/random1.js
@@ -1,74 +1,74 @@
 import React, { useState } from "react";
 
-const RandomCircles =()=>{
-  const [clickCount ,setClickcount]=useState(0)
-  const [circles,setcircle]=useState([])
+const RandomCircles = () => {
+  const [clickCount, setClickCount] = useState(0);
+  const [circles, setCircles] = useState([]);
 
+  const color = ["#e05510", "#cce911", "#11e933", "#1120e9", "#14e10f"];
 
-  const color=["#ff9a68","#dceb08","#08eb12","#08c7eb","#4908eb"]
+  const getcolor = () => {
+    const random = Math.floor(Math.random() * color.length);
+    return color[random];
+  };
 
-  const Rondom =()=>{
-    const x =Math.floor(Math.random()*color.length)
-    return color[x]
+  const generateRandomPosition = (size, x, y) => {
+    const color = getcolor();
+    return { x, y, color, size };
+  };
 
-  }
-  const generetrondom =(size,x,y)=>{
-    const color =Rondom()
-    return {x,y,size,color}
-  }
-const handleclick =(e)=>{
-  setClickcount ((prev)=>prev +1)
-  if(clickCount <2){
+  const handleClick = (e) => {
+    setClickCount((prev) => prev + 1);
+    if (clickCount < 2) {
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
 
-    const mouseX = e.clientX
-    const mouseY = e.clientY
+      const containerWidth = window.innerWidth;
+      const containerHeight = window.innerHeight;
 
-    const containerwidth = window.innerHeight
-    const continer1higth = window.innerWidth
+      const xPercent = (mouseX / containerWidth) * 100;
+      const yPercent = (mouseY / containerHeight) * 100;
 
-    const xparent =(mouseX / containerwidth)*100
-    const Yparent =(mouseY / continer1higth)*100
+      const circleSize = Math.floor(Math.random() * 30) + 20; 
 
-    const circleSize =Math.floor(Math.random()*30)+20
-    const newcircle =generetrondom (circleSize,xparent,Yparent)
+      const newCircle = generateRandomPosition(circleSize, xPercent, yPercent);
 
-    setcircle ((preveces) =>[...preveces,newcircle])
-  }
-    if(clickCount ===2){
-      setClickcount(0);
-      setcircle([])
-    
-  }
-}
+      setCircles((prevCircles) => [...prevCircles, newCircle]);
+    }
 
+    if (clickCount === 2) {
+      setClickCount(0);
+      setCircles([]); 
+    }
+  };
 
   return (
-    <div style={{
-      width:"100vw",
-      height:"100vh",
-      backgroundColor:"white",
-      overflow:"hidden",
-      cursor:"pointer",
-
-    }}
-       onClick={handleclick}
-     > {circles.map((circle,index)=>(
+    <div
+      style={{
+        width: "100vw", 
+        height: "100vh", 
+        background: "#f0f0f0",
+        overflow: "hidden",
+        cursor: "pointer",
+      }}
+      onClick={handleClick}
+    >
+      {circles.map((circle, index) => (
         <div
-        key={index}
-        style={{
-          position:"absolute",
-          top:`${circle.y}%`,
-          left:`${circle.x}%`,
-          width:`${circle.size}vh`,
-          height:`${circle.size}vh`,
-          backgroundColor:circle.color,
-          borderRadius:"50%",
-          transform:"translate(-50% ,-50%)",
-        }}
+          key={index}
+          style={{
+            position: "absolute",
+            top: `${circle.y}%`,
+            left: `${circle.x}%`,
+            width: `${circle.size}vh`, 
+            height: `${circle.size}vh`,
+            backgroundColor: circle.color,
+            borderRadius: "50%",
+            transform: "translate(-50%, -50%)", 
+          }}
         ></div>
       ))}
-      </div>
-    
-  )
-}
+    </div>
+  );
+};
+
 export default RandomCircles;
